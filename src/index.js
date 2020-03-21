df1311b3e8baadeeebff20be4bf0e9a172786529
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-03-21 13:47:08 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let resultConfig = bracketsConfig.map(function(o){
+    return o.join('');
+  })
+  for(let i=0; i<resultConfig.length; i++){
+    if(str.includes(resultConfig[i])){
+      str=str.replace(resultConfig[i], '')
+      i=-1;
+    }
+  }
+  // console.log(resultConfig);
+  if(str==='') {
+    return true;
+  }
+  else{
+    return false;
+  }
+  }
